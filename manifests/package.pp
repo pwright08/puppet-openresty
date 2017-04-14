9655dadf83cd6167ce99c76a45c18ae1eb16b5e1
jayfitz <j.fitzsimmons@kainos.com>
2017-04-14 12:52:29 +0100

    re-ordering openresty repo addition

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,18 +1,16 @@
 
 class openresty::package inherits openresty::params {
 
-
-  package { 'openresty':
-    ensure => present,
-  }
-
-
   yumrepo { 'OpenRestyRepository':
     ensure   => 'present',
     baseurl  => 'https://copr-be.cloud.fedoraproject.org/results/openresty/openresty/epel-$releasever-$basearch/',
     enabled  => 1,
     gpgcheck => 1,
     gpgkey   => 'https://copr-be.cloud.fedoraproject.org/results/openresty/openresty/pubkey.gpg'
+  }
+
+  package { 'openresty':
+    ensure => present,
   }
 
 }
